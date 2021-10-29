25d67bb089553569fe501e6fc4190635a1e5d43b
andreisemianiuk <semeniukandrei677@gmail.com>
2021-10-29 21:43:57 +0300

    Add contacts information   Add google map

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
-import Layout from '../components/Layout/Layout'
+import Layout from '../components/Layout'
+import {contactName, container, description, info, name, title,map} from '../styles/contacts.module.css'
+import MyMap from '../components/MyMap'
 
-const ContactsPage = () => {
+export default function ContactsPage() {
   return (
     <Layout>
-      Contacts Page
+      <section className={container}>
+        <div className={description}>
+          <h3 className={title}>Контактная информация</h3>
+          <div className={contactName}>Наш адрес:</div>
+          <p className={info}>220073, Республика Беларусь<br/>г. Минск ул. Гусовского 2а</p>
+          <div className={contactName}>Телефон:</div>
+          <p className={info}>+375 (17) 202-23-06<br/>+375 (17) 202-23-52<br/>+375 (29) 662-30-04</p>
+          <div className={contactName}>Email:</div>
+          <p className={info}>info@ecosiprom.com</p>
+          <p className={name}>ООО "Экосипром"</p>
+        </div>
+        <div className={map}>
+          <MyMap />
+        </div>
+      </section>
     </Layout>
   )
 }
-
-export default ContactsPage
