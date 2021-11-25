70783b3d8478a8b8ee50b56377408a93ac33a797
andreisemianiuk <semeniukandrei677@gmail.com>
2021-11-25 22:55:47 +0300

    Add data to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,55 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import {graphql} from 'gatsby'
+import parse, {domToReact} from 'html-react-parser'
 
-const HomePage = () => {
+const HomePage = ({data}) => {
+  const {content} = data.allWpContentNode.edges[0].node
+  
+  const options = {
+    replace: (domNode) => {
+      if (domNode.name === 'li') {
+        return <li style={styles.li}>{domToReact(domNode.children,options)}</li>
+      }
+      if (domNode.name === 'ul') {
+        return <ul style={styles.ul}>{domToReact(domNode.children,options)}</ul>
+      }
+      if (domNode.name === 'figure') {
+        return null
+      }
+    }
+  }
+  
   return (
     <Layout>
-      Content of main page
+      {/*<img/>*/}
+      {parse(content,options)}
     </Layout>
   )
 }
 
 export default HomePage
+
+export const pageQuery = graphql`
+  query {
+    allWpContentNode(filter: {slug: {eq: "main"}}) {
+      edges {
+        node {
+          ... on WpPage {
+            id
+            content
+          }
+        }
+      }
+    }
+}
+`
+
+const styles = {
+  li: {
+    // color: 'red'
+  },
+  ul: {
+    // display: 'flex'
+  }
+}
