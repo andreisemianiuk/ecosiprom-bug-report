fdb64681d00f660e276a31ad4220e69cbf6b1e24
andreisemianiuk <semeniukandrei677@gmail.com>
2021-11-27 11:26:50 +0300

    Add wordpress-source menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,51 @@
 import React from 'react'
-import {Link} from 'gatsby'
-import {nav, navItem, navList,navLink,navActiveLink} from '../styles/navbar.module.css'
+import {graphql, Link, useStaticQuery} from 'gatsby'
+import {nav, navActiveLink, navItem, navLink, navList} from '../styles/navbar.module.css'
 
 export const Navbar = () => {
+  const {
+    wpMenu: {
+      menuItems: {nodes},
+    },
+  } = useStaticQuery(graphql`
+    query NavbarQuery {
+      wpMenu {
+        menuItems {
+          nodes {
+            path
+            id
+            label
+          }
+        }
+      }
+    }
+  `)
   return <nav className={nav}>
     <ul className={navList}>
-      <li className={navItem}>
-        <Link to={'/'} className={navLink} activeClassName={navActiveLink}>Главная</Link>
-      </li>
-      <li className={navItem}>
-        <Link to={'/services'} className={navLink} activeClassName={navActiveLink}>Услуги</Link>
-      </li>
-       <li className={navItem}>
-        <Link to={'/catalog'} className={navLink} activeClassName={navActiveLink}>Каталог</Link>
-      </li>
-      <li className={navItem}>
-        <Link to={'/projects'} className={navLink} activeClassName={navActiveLink}>Проекты</Link>
-      </li>
-      <li className={navItem}>
-        <Link to={'/about'} className={navLink} activeClassName={navActiveLink}>О нас</Link>
-      </li>
-      <li className={navItem}>
-        <Link to={'/contacts'} className={navLink} activeClassName={navActiveLink}>Контакты</Link>
-      </li>
+      {
+        nodes.map(({id, label, path}, i) =>
+          i > 0 &&
+          (<li key={id} className={navItem}>
+            <Link to={`${path}`} className={navLink} activeClassName={navActiveLink}>
+              {label}
+            </Link>
+          </li>))
+      }
+      {/*<li className={navItem}>*/}
+      {/*  <Link to={'/services'} className={navLink} activeClassName={navActiveLink}>Услуги</Link>*/}
+      {/*</li>*/}
+      {/*<li className={navItem}>*/}
+      {/*  <Link to={'/catalog'} className={navLink} activeClassName={navActiveLink}>Каталог</Link>*/}
+      {/*</li>*/}
+      {/*<li className={navItem}>*/}
+      {/*  <Link to={'/projects'} className={navLink} activeClassName={navActiveLink}>Проекты</Link>*/}
+      {/*</li>*/}
+      {/*<li className={navItem}>*/}
+      {/*  <Link to={'/about'} className={navLink} activeClassName={navActiveLink}>О нас</Link>*/}
+      {/*</li>*/}
+      {/*<li className={navItem}>*/}
+      {/*  <Link to={'/contacts'} className={navLink} activeClassName={navActiveLink}>Контакты</Link>*/}
+      {/*</li>*/}
     </ul>
   </nav>
-} 
+}
