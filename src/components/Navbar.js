b65c7553d9c86262dbd0cc61622469817a7eb355
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-14 15:23:07 +0300

    Rollback changes for visualization work template

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,30 +22,30 @@ export const Navbar = () => {
   `)
   return <nav className={nav}>
     <ul className={navList}>
-      {
-        nodes.map(({id, label, path}, i) =>
-          i > 0 &&
-          (<li key={id} className={navItem}>
-            <Link to={`${path}`} className={navLink} activeClassName={navActiveLink}>
-              {label}
-            </Link>
-          </li>))
-      }
-      {/*<li className={navItem}>*/}
-      {/*  <Link to={'/services'} className={navLink} activeClassName={navActiveLink}>Услуги</Link>*/}
-      {/*</li>*/}
-      {/*<li className={navItem}>*/}
-      {/*  <Link to={'/catalog'} className={navLink} activeClassName={navActiveLink}>Каталог</Link>*/}
-      {/*</li>*/}
-      {/*<li className={navItem}>*/}
-      {/*  <Link to={'/projects'} className={navLink} activeClassName={navActiveLink}>Проекты</Link>*/}
-      {/*</li>*/}
-      {/*<li className={navItem}>*/}
-      {/*  <Link to={'/about'} className={navLink} activeClassName={navActiveLink}>О нас</Link>*/}
-      {/*</li>*/}
-      {/*<li className={navItem}>*/}
-      {/*  <Link to={'/contacts'} className={navLink} activeClassName={navActiveLink}>Контакты</Link>*/}
-      {/*</li>*/}
+      {/*{*/}
+      {/*  nodes.map(({id, label, path}, i) =>*/}
+      {/*    i > 0 &&*/}
+      {/*    (<li key={id} className={navItem}>*/}
+      {/*      <Link to={`${path}`} className={navLink} activeClassName={navActiveLink}>*/}
+      {/*        {label}*/}
+      {/*      </Link>*/}
+      {/*    </li>))*/}
+      {/*}*/}
+      <li className={navItem}>
+        <Link to={'/services'} className={navLink} activeClassName={navActiveLink}>Услуги</Link>
+      </li>
+      <li className={navItem}>
+        <Link to={'/catalog'} className={navLink} activeClassName={navActiveLink}>Каталог</Link>
+      </li>
+      <li className={navItem}>
+        <Link to={'/projects'} className={navLink} activeClassName={navActiveLink}>Специализация и Проекты</Link>
+      </li>
+      <li className={navItem}>
+        <Link to={'/about'} className={navLink} activeClassName={navActiveLink}>О нас</Link>
+      </li>
+      <li className={navItem}>
+        <Link to={'/contacts'} className={navLink} activeClassName={navActiveLink}>Контакты</Link>
+      </li>
     </ul>
   </nav>
 }
