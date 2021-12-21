7bf194b2a6d1fde38b8811c59a03a68cc927d2a1
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-21 23:08:31 +0300

    Add query for partners logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import parse, {domToReact} from 'html-react-parser'
 import {Slideshow} from '../components/Slideshow'
 import styled from 'styled-components'
 import {devices} from '../common/MediaQuery/media-query'
+import {GatsbyImage} from 'gatsby-plugin-image'
 
 const SliderContainer = styled.div`
   display: flex;
@@ -17,7 +18,7 @@ const SliderContainer = styled.div`
 const MainServicesList = styled.ul`
   color: #ffffff;
   background-color: #00637f;
-  padding: 20px 5px;
+  padding: 5px 5px 10px;
   margin: 20px 0;
   @media ${devices.mobileL} {
     padding: 0;
@@ -28,12 +29,12 @@ const MainServicesItem = styled.li`
   font-weight: bold;
   max-width: 700px;
   padding-left: 50px;
-  margin-bottom: 20px;
+  margin: 20px 0;
   list-style: none;
   @media ${devices.mobileL} {
     font-size: 1.3em;
     padding: 10px 10px 10px 20px;
-    margin-bottom: 0;
+    margin: 0;
   }
 `
 const MainServicesUnderLine = styled.div`
@@ -41,6 +42,7 @@ const MainServicesUnderLine = styled.div`
   max-width: 650px;
   height: 10px;
   background: linear-gradient(116deg, #d3f5f5 0%, #f9fbfd 98%, transparent 98%, transparent 100%);
+
   &:last-child {
     display: none;
   }
@@ -68,7 +70,6 @@ const ServiceItem = styled.div`
     max-width: 100%;
     &:hover {
       transform: scale(1);
-      //box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
     }
   }
 `
@@ -86,26 +87,30 @@ const ServicesTitle = styled.p`
   padding: 0 5px;
 `
 const ServicesList = styled.ul`
-  // color: #ffffff;
-  // background-color: #00637f;
   padding: 0 10px 0 30px;
-  // margin: 0;
-  @media ${devices.mobileL} {
-    //text-align: center;
-  }
 `
 const ServicesListItem = styled.li`
   font-size: 1.1em;
-  //list-style: none;
-  //text-align: center;
+`
+const PartnersList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  //align-items: center;
+  flex-wrap: wrap;
+`
+let PartnerItem = styled.div`
+  max-height: 50px;
+  //width: 200px;
 `
 const HomePage = ({data}) => {
-  const {content} = data.allWpContentNode.edges[0].node
+  let {content} = data.allWpContentNode.edges[0].node
+  let logos = data.allImageSharp.nodes.map(node => node.fluid)
+  console.log('logos >> ', logos)
   
   const options = {
     replace: (domNode) => {
       if (domNode.attribs && domNode.attribs.class === 'main-services') {
-        return <MainServicesList>{domToReact(domNode.children, options)}</MainServicesList>
+        return (<MainServicesList>{domToReact(domNode.children, options)}</MainServicesList>)
       }
       if (domNode.attribs && domNode.attribs.class === 'main-services-item') {
         return (
@@ -136,15 +141,31 @@ const HomePage = ({data}) => {
       if (domNode.attribs && domNode.attribs.class === 'services-list-item') {
         return <ServicesListItem>{domToReact(domNode.children, options)}</ServicesListItem>
       }
+      // if (domNode.attribs && domNode.attribs.class === 'partner-item') {
+      //   console.log('partner item domNode.children >> ', domNode.children)
+      //   return <PartnerItem>{domToReact(domNode.children, options)}</PartnerItem>
+      // }
+      // if (domNode.attribs && domNode.attribs.class === 'partners-container') {
+      //   return <PartnersList>{domToReact(domNode.children, options)}</PartnersList>
+      // }
+      
     },
   }
-  
   return (
     <Layout>
       <SliderContainer>
         <Slideshow autoplay={true}/>
       </SliderContainer>
       {parse(content, options)}
+      <PartnersList>
+        {
+          logos.map(logo => (<PartnerItem><GatsbyImage
+            key={logo.src}
+            alt={logo.src}
+            image={{images: {fallback: {src: logo.src}}, layout: 'constrained', height: 50}}
+          /></PartnerItem>))
+        }
+      </PartnersList>
     </Layout>
   )
 }
@@ -160,6 +181,13 @@ export const pageQuery = graphql`
             id
             content
           }
+        }
+      }
+    }
+    allImageSharp(filter: {fluid: {src: {regex: "/logo/"}}}) {
+      nodes {
+        fluid(fit: FILL, cropFocus: CENTER) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
