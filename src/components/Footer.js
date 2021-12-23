fa689e046c3dc87a8cdd13fa8524c99817905788
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-23 21:22:17 +0300

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterContainer = styled.footer`
+  height: 100px;
+  color: #c5fcff;
+  background-color: #00637f;
+`
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+      Footer Information
+    </FooterContainer>
+  )
+}
