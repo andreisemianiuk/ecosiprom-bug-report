e836be91eff195f4a24ed102fe2b761b0f292200
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-24 09:40:44 +0300

    Add query for fetching data from WP

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,34 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import {graphql} from 'gatsby'
 
-const AboutPage = () => {
+import parse from 'html-react-parser'
+
+const AboutPage = ({data}) => {
+  console.log('data >> ', data)
+  const { content } = data.allWpContentNode.edges[0].node
   return (
     <Layout>
       About Page
+      <div>{parse(content)}</div>
     </Layout>
   )
 }
 
 export default AboutPage
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allWpContentNode(filter: {slug: {eq: "installation-supervision"}}) {
+      edges {
+        node {
+          ... on WpPage {
+            id
+            content
+          }
+        }
+      }
+    }
+}
+
+`
