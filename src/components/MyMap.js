6e5c04f760da3bfd46999e66466980e8725e5a9b
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-24 12:02:53 +0300

    Add map sizes for different devices

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import {useMediaQuery} from 'react-responsive'
 
 export default function MyMap() {
+  const isMobile = useMediaQuery({query: '(max-width: 500px)'})
   if (typeof window !== 'undefined') {
     const position = [53.90100435, 27.516731049346248]
     return (
       <MapContainer
-        style={{height: '70vh', width: '50vw'}}
+        style={isMobile ? {height: '300px', width: '100vw'} : {height: '60vh', width: '50vw'}}
         center={position}
         zoom={15}
         scrollWheelZoom={false}
