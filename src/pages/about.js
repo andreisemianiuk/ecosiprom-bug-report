1303554c05a2e0093676e22765f68d59a95f3868
andreisemianiuk <semeniukandrei677@gmail.com>
2021-12-27 22:09:20 +0300

    Add data from WP   Add styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,60 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import {graphql} from 'gatsby'
+import parse, {domToReact} from 'html-react-parser'
+import styled from 'styled-components'
+import {devices} from '../common/MediaQuery/media-query'
 
-import parse from 'html-react-parser'
+let AboutContainer = styled.div`
+  padding: 50px 30px;
+  @media ${devices.mobileL} {
+    padding: 0 10px 20px;
+  }
+`
+let AboutInfo = styled.p`
+  @media ${devices.mobileL} {
+    text-align: center;
+  }
+`
+let AboutListItem = styled.li`
+  margin-left: 30px;
+`
+let LicenseContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding-bottom: 30px;
+`
+let LicenseItem = styled.div`
+  width: 250px;
+  @media ${devices.mobileL} {
+    width: 150px;
+  }
+`
 
 const AboutPage = ({data}) => {
-  console.log('data >> ', data)
-  const { content } = data.allWpContentNode.edges[0].node
+  const {content} = data.allWpContentNode.edges[0].node
+  const options = {
+    replace: (domNode) => {
+      if (domNode.attribs && domNode.attribs.class === 'about-container') {
+        return (<AboutContainer>{domToReact(domNode.children, options)}</AboutContainer>)
+      }
+      if (domNode.attribs && domNode.attribs.class === 'about-info') {
+        return (<AboutInfo>{domToReact(domNode.children, options)}</AboutInfo>)
+      }
+      if (domNode.attribs && domNode.attribs.class === 'about-list-item') {
+        return (<AboutListItem>{domToReact(domNode.children, options)}</AboutListItem>)
+      }
+      if (domNode.attribs && domNode.attribs.class === 'license-container') {
+        return (<LicenseContainer>{domToReact(domNode.children, options)}</LicenseContainer>)
+      }
+      if (domNode.attribs && domNode.attribs.class === 'license-item') {
+        return (<LicenseItem>{domToReact(domNode.children, options)}</LicenseItem>)
+      }
+    },
+  }
   return (
     <Layout>
-      About Page
-      <div>{parse(content)}</div>
+      <div>{parse(content, options)}</div>
     </Layout>
   )
 }
@@ -19,7 +63,7 @@ export default AboutPage
 
 export const pageQuery = graphql`
   query MyQuery {
-    allWpContentNode(filter: {slug: {eq: "installation-supervision"}}) {
+    allWpContentNode(filter: {slug: {eq: "about-us"}}) {
       edges {
         node {
           ... on WpPage {
