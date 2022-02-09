a13af72ba30cf2ab9e1ab05effc0ba91b8b71bb1
Andrei Semianiuk <semeniukandrei677@gmail.com>
2022-02-09 08:10:02 +0300

    Add backgroun images

--- src/components/ServicesBackgroundImage.js
+++ src/components/ServicesBackgroundImage.js
@@ -1,48 +1,51 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { getImage, GatsbyImage } from "gatsby-plugin-image"
-
-
-function ServicesBackgroundImage ({imageTitle}) {debugger
-  // let {
-  //   allWpMediaItem: {
-  //     nodes
-  //   },
-  // } = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       allWpMediaItem(filter: {title: {eq: "/naladka-3/"}}) {
-  //         nodes {
-  //           id
-  //           title
-  //           localFile {
-  //             childImageSharp {
-  //               gatsbyImageData
-  //             }
-  //           }
-  //           altText
-  //         }
-  //       }
-  //     }
-  //   `
-  // )
-  // console.log('nodes >> ',nodes)
-  // const image = getImage(data.allWpMediaItem.nodes[0].localFile.childImageSharp.gatsbyImageData)
-  
-  // Use like this:
-  // const bgImage = convertToBgImage(image)
-  // return (
-  //   <BackgroundImage
-  //     Tag="section"
-  //     // Spread bgImage into BackgroundImage:
-  //     {...bgImage}
-  //     preserveStackingContext
-  //   >
-  //     <div style={{minHeight: 1000, minWidth: 1000}}>
-  //       {/*<GatsbyImage image={image} alt={"testimage"}/>*/}
-  //     </div>
-  //   </BackgroundImage>
-  // )
-  return <div>Back Image</div>
+import BackgroundImage from 'gatsby-background-image'
+import { getImage } from 'gatsby-plugin-image'
+import { convertToBgImage } from 'gbimage-bridge'
+import styled from 'styled-components'
+// import { useBgQuery } from './useBgQuery'
+function BackImage({ className, description, children }) {
+  let {
+    allWpMediaItem: { nodes },
+  } = useStaticQuery(
+    graphql`
+      query {
+        allWpMediaItem(filter: { title: { regex: "/back/" } }) {
+          nodes {
+            id
+            title
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+              }
+            }
+            altText
+          }
+        }
+      }
+    `
+  )
+  // console.log('nodes >> ', nodes)
+  let currentImage = nodes.find(v => v.altText === description)
+  // console.log('images >> ', images)
+  let image = getImage(currentImage.localFile.childImageSharp.gatsbyImageData)
+  let bgImage = convertToBgImage(image)
+  return (
+    <BackgroundImage
+      className={className}
+      Tag='section'
+      // Spread bgImage into BackgroundImage:
+      {...bgImage}
+      preserveStackingContext>
+      {children}
+    </BackgroundImage>
+  )
 }
+
+const ServicesBackgroundImage = styled(BackImage)`
+  background-position: center;
+  background-size: cover;
+`
+
 export default ServicesBackgroundImage
