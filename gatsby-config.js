6b860b166772b3d6df39fbbcc1f6d657043f81cd
Andrei Semianiuk <semeniukandrei677@gmail.com>
2022-03-01 20:25:39 +0300

    Add plugin for modal   Add plugin for svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,25 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-plugin-modal-routing`,
+      options: {
+        // A selector to set react-modal's app root to, default is `#___gatsby`
+        // See http://reactcommunity.org/react-modal/accessibility/#app-element
+        appElement: '#___gatsby',
+
+        // Object of props that will be passed to the react-modal container
+        // See http://reactcommunity.org/react-modal/#usage
+        modalProps: { isOpen: false },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
   ],
 }
