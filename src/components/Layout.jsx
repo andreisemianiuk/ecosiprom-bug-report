229cd34cb88a73e45527de3ec155ccbd8743b6b3
andreisemianiuk <semeniukandrei677@gmail.com>
2022-06-12 22:24:26 +0300

    Implement header components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,41 +1,18 @@
-import { Link } from 'gatsby'
-import * as React from 'react'
-import styled from 'styled-components'
-import { ContactHeader } from './ContactHeader'
-import Footer from './Footer'
-import { Logo } from './Logo'
-import { Navbar } from './Navbar'
+import * as React from "react"
+import styled from "styled-components"
+import Footer from "./Footer"
+import Header from "./header/Header"
 
-const Container = styled.div``
 const MainContent = styled.div`
   min-height: 80vh;
 `
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 20px;
-  @media (max-width: 950px) {
-    flex-direction: column;
-  }
-  @media (max-width: 500px) {
-    margin: 15px 0;
-  }
-`
-const StyledLink = styled(Link)``
 
 export default function Layout({ children }) {
   return (
-    <Container>
-      <Header>
-        <StyledLink to={'/'}>
-          <Logo />
-        </StyledLink>
-        <ContactHeader />
-        <Navbar />
-      </Header>
+    <>
+      <Header />
       <MainContent>{children}</MainContent>
       <Footer />
-    </Container>
+    </>
   )
 }
