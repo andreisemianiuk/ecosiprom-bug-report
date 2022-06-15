7d21a58bf1d78aa6660987a6ac9f89b9fe866885
andreisemianiuk <semeniukandrei677@gmail.com>
2022-06-15 17:59:09 +0300

    Add primary button

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -4,23 +4,24 @@ import styled from "styled-components"
 const StyledButton = styled.button`
   width: 173px;
   height: 48px;
-  padding: 18px, 30px, 18px, 30px;
+  padding: 14px 30px;
   font-size: 14px;
   font-weight: 600;
   line-height: 20px;
+  gap: 8px;
   color: #ffffff;
   background-color: #0e6683;
   border: none;
   border-radius: 2px;
   cursor: pointer;
+  &:hover {
+    background-color: #1a7897;
+    transition: background-color 0.3s ease-in-out;
+  }
 `
 
-const PrimaryButton = ({ ...props }) => {
-  return (
-    <StyledButton {...props} onClick={() => console.log("button clicked")}>
-      Оставить заявку
-    </StyledButton>
-  )
+const PrimaryButton = ({ text, callback }) => {
+  return <StyledButton onClick={callback}>{text}</StyledButton>
 }
 
 export default PrimaryButton
