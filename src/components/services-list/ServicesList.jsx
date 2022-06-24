fdbf7f3b3ab2829ef46b8a93447a72e90af953dc
andreisemianiuk <semeniukandrei677@gmail.com>
2022-06-24 13:23:06 +0300

    Decouple ServiceList component

--- src/components/services-list/ServicesList.jsx
+++ src/components/services-list/ServicesList.jsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-import { graphql, Link, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
-import { getImage } from "gatsby-plugin-image";
-import { convertToBgImage } from "gbimage-bridge";
-import BackgroundImage from "gatsby-background-image";
-import parse, { domToReact } from "html-react-parser";
-import SecondaryButton from "../buttons/SecondaryButton";
+import PrimaryButton from "../buttons/PrimaryButton";
+import ServiceBox from "./ServiceBox";
 
 const ContentWrapper = styled.section`
   max-width: 1170px;
   margin: 0 auto;
+  padding-bottom: 80px;
 `;
 const Title = styled.h1`
   line-height: 48px;
@@ -23,70 +21,10 @@ const ItemsWrapper = styled.div`
 
   justify-content: center;
 `;
-const Item = styled(Link)`
-  width: 100%;
-  height: 100%;
-  background: ${({ hovered }) =>
-    hovered
-      ? // ? "linear-gradient(180deg, rgba(34, 44, 56, 0.4) 0%, rgba(33, 44, 56, 0.8) 100%)"
-        "rgba(34, 44, 56, 0.4)"
-      : "rgba(0, 0, 0, 0.1)"};
-  border-radius: 2px;
-  overflow: hidden;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-`;
-const Content = styled.div`
+const ButtonWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-`;
-const BackgroundImageContainer = styled(BackgroundImage)`
-  width: 100%;
-  height: 100%;
-`;
-const InfoBox = styled.div`
-  min-height: 108px;
-  padding-top: 20px;
-  padding-left: 20px;
-  margin: 20px;
-  background-color: #fff;
-  border-radius: 2px;
-  overflow: hidden;
-`;
-const ItemTitle = styled.h3`
-  font-weight: 700;
-  color: ${({ hovered }) => (hovered ? "#0e6683" : "initial")};
-  margin-top: 0;
-  transition: color 0.3s ease-in-out;
-  &::first-letter {
-    text-transform: capitalize;
-  }
-`;
-
-const ItemList = styled.ul`
-  max-height: ${({ hovered }) => (hovered ? "350px" : "0")};
-  overflow: ${({ hovered }) => (hovered ? "visible" : "hidden")};
-  opacity: ${({ hovered }) => (hovered ? 1 : 0)};
-  margin: 0;
-  padding-bottom: 20px;
-  transition: all 0.7s ease-in-out;
-
-  font-size: 15px;
-  line-height: 22px;
-  font-weight: 400;
-  color: #4a5763;
-`;
-const SubItem = styled.li`
-  margin-left: 40px;
-  &::first-letter {
-    text-transform: capitalize;
-  }
-  &::marker {
-    font-size: 0.6em;
-  }
+  justify-content: center;
+  margin-top: 30px;
 `;
 
 const ServicesList = () => {
@@ -113,14 +51,6 @@ const ServicesList = () => {
     }
   `);
 
-  let options = {
-    replace: domNode => {
-      if (domNode.name === "li") {
-        return <SubItem>{domToReact(domNode.children, options)}</SubItem>;
-      }
-    },
-  };
-
   const handleHoverOn = index => {
     setHoveredItemId(index);
   };
@@ -133,37 +63,20 @@ const ServicesList = () => {
       <Title>Наши услуги</Title>
       <ItemsWrapper>
         {nodes.map(service => {
-          let image = getImage(
-            service.localFile.childImageSharp.gatsbyImageData
-          );
-          let bgImage = convertToBgImage(image);
           let hovered = hoveredItemId === service.id ? true : false;
-
           return (
-            <Item
-              key={service.id}
-              onMouseOver={() => handleHoverOn(service.id)}
-              onMouseLeave={handleHoverOff}
-              hovered={hovered}>
-              <BackgroundImageContainer
-                Tag="div"
-                // Spread bgImage into BackgroundImage:
-                {...bgImage}
-                preserveStackingContext>
-                <Content>
-                  <InfoBox>
-                    <ItemTitle hovered={hovered}>{service.altText}</ItemTitle>
-                    <ItemList hovered={hovered}>
-                      {parse(service.description, options)}
-                      <SecondaryButton title="Подробнее" hovered={true} />
-                    </ItemList>
-                  </InfoBox>
-                </Content>
-              </BackgroundImageContainer>
-            </Item>
+            <ServiceBox
+              handleHoverOn={handleHoverOn}
+              handleHoverOff={handleHoverOff}
+              service={service}
+              hovered={hovered}
+            />
           );
         })}
       </ItemsWrapper>
+      <ButtonWrapper>
+        <PrimaryButton text={"Оставить заявку"} />
+      </ButtonWrapper>
     </ContentWrapper>
   );
 };
