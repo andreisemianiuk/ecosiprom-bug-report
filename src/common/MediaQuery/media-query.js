0e0569d7eeca483971b0850f56e85dc707a44468
andreisemianiuk <semeniukandrei677@gmail.com>
2022-06-24 12:08:31 +0300

    Update width for desktop

--- src/common/MediaQuery/media-query.js
+++ src/common/MediaQuery/media-query.js
@@ -1,12 +1,12 @@
-const sizes = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px',
-}
+export const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "1920px",
+};
 export const devices = {
   mobileS: `(max-width: ${sizes.mobileS})`,
   mobileM: `(max-width: ${sizes.mobileM})`,
@@ -15,4 +15,4 @@ export const devices = {
   laptop: `(max-width: ${sizes.laptop})`,
   laptopL: `(max-width: ${sizes.laptopL})`,
   desktop: `(max-width: ${sizes.desktop})`,
-}
+};
