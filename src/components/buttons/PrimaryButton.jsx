b47c03d7df73311c34ab186670630e9fa6cf9222
andreisemianiuk <semeniukandrei677@gmail.com>
2022-07-07 21:00:32 +0300

    Add additional features for primary button

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,44 +1,58 @@
 import * as React from "react";
 import styled from "styled-components";
+import RightArrowIcon from "../../assets/right-arrow.svg";
 
 const StyledButton = styled.button`
-  width: 173px;
+  width: ${(props) => (props.width ? props.width : `173`)}px;
   height: 48px;
   padding: 14px 18px;
   font-size: 14px;
   font-weight: 600;
   line-height: 20px;
   gap: 8px;
-  color: ${props => (props.color ? props.color : `#ffffff`)};
-  background-color: ${props =>
+  color: ${(props) => (props.color ? props.color : `#ffffff`)};
+  background-color: ${(props) =>
     props.backgroundColor ? props.backgroundColor : `#0e6683`};
   border: none;
   border-radius: 2px;
   cursor: pointer;
   transition: background-color 0.3s ease-in-out;
   &:hover {
-    background-color: ${props =>
+    background-color: ${(props) =>
       props.hoverStyles?.backgroundColor
         ? props.hoverStyles?.backgroundColor
         : `#1a7897`};
-    color: ${props => props.hoverStyles?.color && props.hoverStyles?.color};
+    color: ${(props) => props.hoverStyles?.color && props.hoverStyles?.color};
   }
+`;
+
+const Text = styled.div`
+  position: relative;
+`;
+const StyledArrowIcon = styled(RightArrowIcon)`
+  margin-left: 10px;
 `;
 
 const PrimaryButton = ({
   text,
+  width,
   callback,
   color,
   backgroundColor,
   hoverStyles,
+  isRightArrow,
 }) => {
   return (
     <StyledButton
       onClick={callback}
       color={color}
+      width={width}
       backgroundColor={backgroundColor}
       hoverStyles={hoverStyles}>
-      {text}
+      <Text>
+        <span>{text}</span>
+        {isRightArrow && <StyledArrowIcon />}
+      </Text>
     </StyledButton>
   );
 };
