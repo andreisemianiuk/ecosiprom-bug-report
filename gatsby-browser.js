402dca8a13fceb36c26786591b7adce583da6281
andreisemianiuk <semeniukandrei677@gmail.com>
2022-07-12 19:20:44 +0300

    Prevent from scrolling to top

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,3 +15,15 @@ import { CatalogMenuProvider } from "./src/common/catalogMenu/useCatalogMenu";
 export const wrapRootElement = ({ element }) => (
   <CatalogMenuProvider>{element}</CatalogMenuProvider>
 );
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const currentPosition = getSavedScrollPosition(location);
+  // const queriedPosition = getSavedScrollPosition({ pathname: `/random` })
+
+  window.scrollTo(...(currentPosition || [0, 0]));
+
+  return false;
+};
