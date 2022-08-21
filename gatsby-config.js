0554e64a78c4ea7362b7710fa81058729bf840cd
andreisemianiuk <semeniukandrei677@gmail.com>
2022-08-21 15:28:25 +0300

    Remove unused dependencies from package-json

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,12 +29,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-react-leaflet",
-      options: {
-        linkStyles: true, // (default: true) Enable/disable loading stylesheets via CDN
-      },
-    },
-    {
       resolve: `gatsby-source-wordpress`,
       options: {
         /*
