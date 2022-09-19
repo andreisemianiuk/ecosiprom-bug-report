ea8f6789492005dcad65ac318c9f1b0ff255f741
andreisemianiuk <semeniukandrei677@gmail.com>
2022-09-19 23:37:05 +0300

    Add responsive FeedbackForm component

--- src/components/feedback-form/FeedbackForm.jsx
+++ src/components/feedback-form/FeedbackForm.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 import PrimaryButton from "../buttons/PrimaryButton";
 
@@ -9,10 +10,22 @@ const Container = styled.section`
   width: 100%;
   height: 200px;
   background-color: #cfe0e6;
+  @media (max-width: 767px) {
+    height: max-content;
+    padding: 50px 20px;
+    flex-direction: column;
+  }
 `;
 const Info = styled.div`
   max-width: 373px;
   margin-right: 67px;
+  @media (max-width: 767px) {
+    max-width: max-content;
+    margin-right: 0;
+  }
+`;
+const ButtonWrapper = styled.div`
+  width: ${({ isMobile }) => (isMobile ? "100%" : "fit-content")};
 `;
 const Title = styled.h2`
   margin: 0;
@@ -23,9 +36,13 @@ const Text = styled.p`
   margin: 0;
   line-height: 24px;
   color: #4a5763;
+  @media (max-width: 767px) {
+    margin-bottom: 30px;
+  }
 `;
 
 const FeedbackForm = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
     <Container>
       <Info>
@@ -35,13 +52,16 @@ const FeedbackForm = () => {
           вопросы.
         </Text>
       </Info>
-      <PrimaryButton
-        text={"Получить консультацию"}
-        width={250}
-        isRightArrow
-        pathTo={"/send-form"}
-        state={{ modal: true }}
-      />
+      <ButtonWrapper isMobile={isMobile}>
+        <PrimaryButton
+          isMobile={isMobile}
+          text={"Получить консультацию"}
+          width={250}
+          isRightArrow
+          pathTo={"/send-form"}
+          state={{ modal: true }}
+        />
+      </ButtonWrapper>
     </Container>
   );
 };
