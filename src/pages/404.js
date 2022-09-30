1eeb1659e5f55b0ac52d13a3ea42818d854f4bff
andreisemianiuk <semeniukandrei677@gmail.com>
2022-09-30 21:46:17 +0300

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,24 @@
+import { navigate } from "gatsby";
 import React from "react";
+import styled from "styled-components";
 import Layout from "../components/layout/Layout";
 
-export default function NotFound() {
+export default function NotFound({ location }) {
   return (
-    <Layout>
-      <h1>Page Not Found</h1>
-      <p>Oops, we couldn't find this page!</p>
+    <Layout location={location}>
+      <Container>
+        <h1>Page Not Found</h1>
+        <p>Oops, we couldn't find this page!</p>
+        <button onClick={() => navigate(-1)}>Back to previous page</button>
+      </Container>
     </Layout>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 400px;
+`;
